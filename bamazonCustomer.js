45235b6b1ae2a0ebcd01d371ba1ce49c9d173dff
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-06-29 23:03:48 -0400

    minimum requirement challenge 1 met

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,66 @@
 var db = require('./db');
+var inquirer = require('inquirer');
 
+//Set up connection to database
 var connection = db.login();
 connection.connect(function(err){
     if(err){
         throw err; //throw exception
     }
-    displayStock();
-    console.log("done");
+    displayStock(askCustomer);
 });
 
-displayStock =() =>{
-    connection.query(`SELECT * FROM products`);
+displayStock =(callback) =>{
+    //Function displays items from table products
+    connection.query(`SELECT * FROM products`, function (error, results){
+        if (error){
+            throw error;
+        }
+        for (var i=0; i<results.length; i++){
+            console.log(`Item ID: ${results[i].id}\nProduct Name: ${results[i].product_name}\nPrice: ${results[i].price}`);
+            console.log("--------------------------");
+        }
+        callback();
+    });
+}
+
+askCustomer = () =>{
+    //Prompts user which product and how many they wish to buy.
+    //console.log("Ask customer here");
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter the ID of the product you want to buy.",
+            name: "productID"
+        },
+        {
+            type: "input",
+            message: "How may do you want to buy?",
+            name: "amount"
+        }
+    ]).then(answers => {
+        // Use user feedback for... whatever!!
+        connection.query(`SELECT * FROM products WHERE id = ?`, [answers.productID],function (error, results){
+            if (error){
+                throw error;
+            }
+            if (results[0] == null){
+                console.log("Invalid entry! Product not found");
+                //askCustomer();
+                connection.end();
+            }
+            else{
+                var newQuanity = parseInt(results[0].stock_quanity) - parseInt(answers.amount);
+                if (newQuanity < 0){
+                    console.log("Insufficient quantity!");
+                    connection.end();
+                }
+                else{
+                    connection.query(`UPDATE products SET stock_quanity = ? WHERE id = ?`, [newQuanity, answers.productID]);
+                    var total = (answers.amount * parseFloat(results[0].price)).toFixed(2);
+                    console.log(`Order placed, your total is: $ ${total}`);
+                }
+            }
+        });
+    });
 }
--- products.sql
+++ products.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE bamazon;
+use bamazon;
 CREATE TABLE products(
     id int primary key auto_increment,
     product_name VARCHAR(50) not null,
@@ -7,14 +8,14 @@ CREATE TABLE products(
     stock_quanity int default 0
 );
 
-insert into inventory (product_name, department_name, price, stock_quanity) values 
-("Sombrero", "Hats", 1.00, 10);
-("dummy item", "dummy department" 2.00, 10);
-("dummy item2", "dummy department" 2.00, 10);
-("dummy item3", "dummy department" 2.00, 10);
-("dummy item4", "dummy department" 2.00, 10);
-("dummy item5", "dummy department" 2.00, 10);
-("dummy item6", "dummy department" 2.00, 10);
-("dummy item7", "dummy department" 2.00, 10);
-("dummy item8", "dummy department" 2.00, 10);
-("dummy item9", "dummy department" 2.00, 10);
+insert into products (product_name, department_name, price, stock_quanity) values 
+("Sombrero", "Hats", 1.00, 10),
+("Dummy data1", "Dummy Departmant", 1.00, 10),
+("Dummy data2", "Dummy Departmant", 1.00, 10),
+("Dummy data3", "Dummy Departmant", 1.00, 10),
+("Dummy data4", "Dummy Departmant", 1.00, 10),
+("Dummy data5", "Dummy Departmant", 1.00, 10),
+("Dummy data6", "Dummy Departmant", 1.00, 10),
+("Dummy data7", "Dummy Departmant", 1.00, 10),
+("Dummy data8", "Dummy Departmant", 1.00, 10),
+("Dummy data9", "Dummy Departmant", 1.00, 10);
